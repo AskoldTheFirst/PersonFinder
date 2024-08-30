55376564d7e596a19830d02a0c9c58d441241407
vkramar <kramarvladimir@gmail.com>
2024-08-30 14:10:59 +0300

    Creating react app - initial commit.

---
+++ client/src/App.tsx
@@ -0,0 +1,17 @@
+import './App.css'
+import Header from './components/Header'
+import Footer from './components/Footer'
+import Main from './components/Main'
+
+function App() {
+
+  return (
+    <>
+      <Header />
+      <Main />
+      <Footer />
+    </>
+  )
+}
+
+export default App
