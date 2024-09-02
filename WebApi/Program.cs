3f064745ef03228a821400efb83f59e84f0d4706
vkramar <kramarvladimir@gmail.com>
2024-09-02 13:55:34 +0300

    Adding EF DbContext for MongoDb

--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -1,4 +1,8 @@
 
+using WebApi.Database;
+using MongoDB.Driver;
+using Microsoft.EntityFrameworkCore;
+
 namespace WebApi
 {
     public class Program
@@ -13,6 +17,10 @@ namespace WebApi
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
+            builder.Services.AddDbContext<PersonFinderDbContext>(opt =>
+            {
+                opt.UseMongoDB("mongodb://localhost:27017/", "PersonFinderDb");
+            });
 
             var app = builder.Build();
 
@@ -29,6 +37,13 @@ namespace WebApi
 
 
             app.MapControllers();
+
+            // To create initial db.
+            {
+                var client = new MongoClient("mongodb://localhost:27017/");
+                IMongoDatabase database = client.GetDatabase("PersonFinderDb");
+                PersonFinderDbContext.Create(database);
+            }
 
             app.Run();
         }
